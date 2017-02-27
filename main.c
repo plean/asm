91039cd05cfc33c20380afe68503b347d38ede90
plean <jean.plancher@epitech.eu>
2017-02-27 14:47:01 +0100

    alors

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int	my_putchar(char);
+int	my_strlen(char *);
+int	my_putstr(char *);
+int     my_putnbr(int);
+
+int	main(int ac, char **av)
+{
+  while (--ac) printf(" : %d\n", my_putnbr(atoi(av[ac])));
+  return (0);
+}
