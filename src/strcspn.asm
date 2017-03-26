6ab6e064d731bf5fcfb8cdc4e53e26f8c6da6e5f
plean <jean.plancher@epitech.eu>
2017-03-26 14:30:53 +0200

    strcspn + strpbrk

--- src/strcspn.asm
+++ src/strcspn.asm
@@ -1,6 +1,34 @@
         extern  _GLOBAL_OFFSET_TABLE_
 	global strcspn:function
 
+_strlen:
+	mov     rax, -1		; set the return value at -1
+
+_strlen_loop:
+	inc     rax	; increment rax
+	cmp     byte [rdi + rax], 0 ; test if rax element of rdi if not equal to 0
+	jne     _strlen_loop		    ; if not loop
+	ret			    ; else return rax
+
 strcspn:
-	xor     rax, rax ;
-	ret	 ;  
+	;; call	_strlen		; rax = strlen(rdi);
+	xor	rax, rax	; rax = 0;
+        xor     r9, r9		; r9 = 0;
+_loop:
+	cmp     byte [rdi + r9], 0 ; if !rdi[r9]
+	je      _ret		   ;   goto _ret;
+	xor     r8, r8		   ; r8 = 0;
+_loop2:
+	cmp     byte [rsi + r8], 0 ; if !rsi[r8]
+	je      _inc		   ;   goto _inc;
+	mov     rdx, [rsi + r8]	   ; rdx = rsi[r8];
+	cmp     [rdi + r9], dl	   ; if rdi[r9] == (char)rdx
+	je      _ret		   ;   goto _ret;
+	inc     r8		   ; r8++;
+	jmp     _loop2		   ; goto _loop2;
+_inc:
+	inc	rax		; rax++;
+	inc	r9		; r9++;
+	jmp     _loop		; goto _loop;
+_ret:
+	ret	 ; return rax;
--- src/strpbrk.asm
+++ src/strpbrk.asm
@@ -1,6 +1,26 @@
         extern  _GLOBAL_OFFSET_TABLE_
 	global strpbrk:function
 
-strpbrk:
-	xor     rax, rax ;
-	ret	 ;
+strpbrk:	
+	xor	rax, rax	; rax = 0;
+_loop:
+	cmp	byte [rdi + rax], 0 ; if !rdi[rax]
+	je	_ret		    ;   goto _ret;
+	xor	r8, r8		    ; r8 = 0;
+_loop2:	
+	cmp	byte [rsi + r8], 0 ; if !rsi[r8]
+	je	_inc		   ;   goto _inc;
+	mov	rdx, [rsi + r8]	   ; rdx = rsi[r8];
+	cmp	[rdi + rax], dl	   ; if rdi[rax] == (char)rdx
+	je	_found		   ;   goto _found;
+	inc	r8		   ; r8++;
+	jmp	_loop2		   ; goto _loop2;
+_inc:	
+	inc	rax		; rax++;
+	jmp	_loop		; goto _loop;
+_found:
+	add	rax, rdi	; rax += rdi;
+	ret			; return rax;
+_ret:
+	xor	rax, rax	; rax = 0;
+	ret			; return rax
