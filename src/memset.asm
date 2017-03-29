6c775749db1d7506dc3a002e5d0fa2d8c17bcbfd
plean <jean.plancher@epitech.eu>
2017-03-29 18:15:36 +0200

    fix strncmp

--- src/memset.asm
+++ src/memset.asm
@@ -2,14 +2,14 @@
 	global memset:function
 
 memset:				; void *memset(void *rdi, int rsi, size_t rdx);
-	xor rcx, rcx		; rcx = 0;
-	mov rax, rdi		; rax = rdi
+	xor	rcx, rcx	; rcx = 0;
+	mov	rax, rdi	; rax = rdi
 _loop:
-	cmp rdx, rcx		; if (rdx == rcx)
-	jz _end			;   goto _end;
-	mov [rdi], sil		; *rax = (char)rsi;
-	inc rcx			; rcx++;
-	inc rdi			; rax++;
-	jmp _loop		; goto _loop;
+	cmp	rdx, rcx	; if (rdx == rcx)
+	jz	_end		;   goto _end;
+	mov	[rdi], sil	; *rax = (char)rsi;
+	inc	rcx		; rcx++;
+	inc	rdi		; rax++;
+	jmp	_loop		; goto _loop;
 _end:
 	ret			; return rax;
--- src/strncmp.asm
+++ src/strncmp.asm
@@ -7,12 +7,12 @@ strncmp:			; strncmp(char *s1, char *s2, size_t n)
 	xor	r8, r8		; i ^= i <=> i = 0
 	xor	rax, rax	; ret ^= ret <=> ret = 0
 _loop:
-	mov	al, [rdi + r8]	   ; (char)ret = s1[i]
-	cmp	al, [rsi + r8]	   ; if (char)ret != 0
+	cmp	r8, rdx		   ; if i == n
+	je	_return_zero	   ; goto _return_zero
+	mov	byte al, [rdi + r8]; (char)ret = s1[i]
+	cmp	byte al, [rsi + r8]; if (char)ret != 0
 	jne	_upd_val	   ; goto _upd_val
 	cmp	byte [rdi + r8], 0 ; if s1[i] == 0
-	je	_upd_val	   ; goto _upd_val
-	cmp	r8, rdx		   ; if i == n
 	je	_upd_val	   ; goto _upd_val
 	inc	r8		   ; i++
 	jmp	_loop
@@ -21,5 +21,8 @@ _upd_val:
 	cmp	al, 127		; if (char)al <= 127
 	jbe	_return		; goto _return
 	sub	eax, 256	; ret -= 256
+	ret
+_return_zero:	
+	xor	rax, rax
 _return:
 	ret			; return ret
